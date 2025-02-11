e5a51e4dfbec9c1a316b837fed9a8dc1edf2a1ab
abai <459270834@qq.com>
2025-02-11 21:45:41 +0800

    v1

---
+++ JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace WEB.BPM.Entities
+{
+    public class JwtOptions
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpiresIn { get; set; } = 7200;
+    }
+}
